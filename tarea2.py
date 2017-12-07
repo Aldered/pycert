014e41ee54a82edc35dc7e7ee49325127c326f60
Aldered <root@Debiphy>
2017-12-06 23:49:20 -0600

    tarea2.py

---
+++ tarea2.py
@@ -0,0 +1,12 @@
+from random import randint 
+
+def passwd():
+
+	int(min_start) = "a"
+	int(min_end) = "z"
+	int(max_start) = "A"
+	int(max_end) = "Z"
+	num_start = 1
+	num_end = 5
+	return randint(min_start, min_end, max_start, max_end, num_start, num_end)
+
