0b9f267fe5a2b4b5b370e4a7afe5cfb14780eb11
Aldered <root@Debiphy>
2017-12-07 20:26:36 -0600

    tarea1 punto 2 el bueno

--- tarea1p2.py
+++ tarea1p2.py
@@ -1,9 +1,19 @@
 #Cabrera Balderas Carlos Eduardo
 #UNAM-CERT
-
+''' 
+Descripcion: Indica si un numero es primo.
+Recibe: Numero. 
+Devuelve: Cierto o falso.
+'''
 def primo(x):
-
+	''' 
+	Si el numero es mayor o igual a 2 que es el primer primo...
+	'''
 	if x >= 2:
+		''' 
+		Si no da resultado de residuoo imprime falso, de lo contrario
+		verdadero.
+		'''
 		if not x % 2:
 			print 'False'
 		else: 
