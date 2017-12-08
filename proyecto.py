36e551d7a9d9ae26bf55c3dade5f334541cc49e8
Aldered <root@Debiphy>
2017-12-08 06:16:21 -0600

    Avance 2 del proyecto

--- proyecto.py
+++ proyecto.py
@@ -1,10 +1,21 @@
 #Becerra Alvarado Hugo Alonso
 #Cabrera Balderas Carlos Eduardo
 
+from re import search
 from requests import get 
+from requests import options
 from requests.exceptions import ConnectionError
+import re
 import sys
 import optparse
+
+f1=open("reporte.txt",'w')
+
+f_corr = f1.write("Los correos encontrados son: ")
+f_server = f1.write("El servidor es: ")
+f_php = f1.write("La version de PHP que usa es: ")
+f_http = f1.write("Los metodos http que usa son: ")
+f_cms = f1.write("El cms que maneja es:" )
 
 def printError(msg, exit = False):
 	sys.stderr.write ('Error:\t%s\n' % msg)
@@ -13,15 +24,15 @@ def printError(msg, exit = False):
 def addOptions():
 	parser = optparse.OptionParser()
 	parser.add_option('-v','--verbose', dest='verbose', default=None, action= 'store_true', help='Detalles.') 
-	parser.add_option('-c', '--correo', dest='correo', default=None, action='store_true', help='Obtenemos el correo.')
-	parser.add_option('-s', '--server', dest='server', default=None, action='store_true', help='Nos da la version del servidor')
-	parser.add_option('-p', '--python', dest='python', default=None, action="store_true", help="Nos da la version de Python.")
-	parser.add_option('-m', '--http', dest= 'http', default=None, action="store_true", help="Nos da los metodos http usados.")
-	parser.add_option('-C', '--cms', dest='cms', default=None, action="store_true", help="Nos da los cms.")
+	parser.add_option('-c', '--correo', dest='correo', default=None, help='Obtenemos el correo.')
+	parser.add_option('-s', '--server', dest='server', default=None, help='Nos da la version del servidor')
+	parser.add_option('-p', '--php', dest='php', default=None, help="Nos da la version de PHP.")
+	parser.add_option('-m', '--http', dest= 'http', default=None, help="Nos da los metodos http usados.")
+	parser.add_option('-C', '--cms', dest='cms', default=None, help="Nos da los cms.")
 	parser.add_option('-r', '--reporte', dest='reporte', default=None, action="store_true", help="Nos genera un reporte.")
 	parser.add_option('-t', '--tor', dest='tor', default=None, action="store_true", help="Se usara tor.")
 	parser.add_option('-b', '--busqueda', dest='busqueda', default=None, action="store_true", help="Se hara la busqueda.")
-	parser.add_option('-a', '--archivo', dest='archivo', default=None, action="store_true", help="Archivo de configuracion.")
+	parser.add_option('-a', '--archivo', dest='archivo', default=None, help="Archivo de configuracion.")
 	opts,args = parser.parse_args()
 	return opts
 
@@ -33,22 +44,33 @@ if __name__ == '__main__':
 	try:
 		opts = addOptions()
 		checkOption(opts)
-		
-		archivo=open("opts.archivo", 'r'):
-			
-			close(archivo)
+				
+		arch=open(opts.archivo,'r')		
+		for n in arch.readlines():
+			print n
+		arch.close()
+
 		if opts.verbose == True:
-			print "Ingresaste verbose."
+			print "Ingresaste verbose"
 		if opts.correo == True:
-			print "Ingresaste correo."
-		if opts.server == True:
-			print "Ingresaste server."
-		if opts.python == True:
-			print "Ingresaste python."
+			co = "[a-z]+\.*[a-z]*@.*\..*"
+			re1 = get(opts.server, co)
+			print "re1"
+		if opts.server!=False:
+			re_server = get(opts.server).headers['server']
+			print re_server
+			f1.write(re_server)
+		if opts.php == True:
+			re_php = get(opts.php).headers['x-powered-by']
+			print "La version de PHP es: %s " % re_php
+			f1.write(re_php)
 		if opts.http == True:
-			print "Ingresaste http."
+			re_http = get(opts.http).headers['options']
+			print "Los metodos que permite son: %s " % re_http
+			f1.write(re_http)
 		if opts.cms == True:
-			print "Ingresaste CMS."
+			patron = r"meta name=\"generator\" content=\".*\""
+			print 'El cms que maneja es: %s ' % re.search(patron, get(opts.server))
 		if opts.reporte == True:
 			print "Ingresaste reporte."
 		if opts.tor == True:
@@ -59,7 +81,7 @@ if __name__ == '__main__':
 		printError('Ocurrio un error inesperado')
 		printError(e, True)
 
-
+f1.close()
 
 
 
